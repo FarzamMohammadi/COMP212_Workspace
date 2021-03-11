673f844781418e07e99f0ad55e5da9113e265552
FarzamMohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2021-03-11 14:49:02 -0500

    Midterm - Excel $ CovidResults classes complete

---
+++ 301109706(Mohammadi)_Test01/301109706(Mohammadi)_Test01/CovidResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _301109706_Mohammadi__Test01
+{
+    class CovidResult
+    {
+        public string countryRegion { get; set; }
+        public string provinceState { get; set; }
+        public string recordDate { get; set; }
+        public string numberOfRecovered { get; set; }
+
+    }
+}
